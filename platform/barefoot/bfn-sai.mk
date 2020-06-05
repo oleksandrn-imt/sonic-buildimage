718d7e672ba515d9a9e7f20a38fab9847fd62a84
Andrii Kit <andriik@interfacemasters.com>
2020-06-05 15:28:00 +0000

    [T2624]: Use IMT copy of sonic-sairedis repository. Fix sonic build with IMT bfnsdk*.deb package

--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,5 +1,5 @@
-BFN_SAI = bfnsdk_9.0.0.cc6ccbe_pr_deb9.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_9_0/bfnsdk_9.0.0.cc6ccbe_pr_deb9.deb"
+BFN_SAI = bfnsdk_1.0.0_amd64.deb
+$(BFN_SAI)_URL = "http://192.168.157.83/yuriih/barefoot/$(BFN_SAI)"
 
 SONIC_ONLINE_DEBS += $(BFN_SAI)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
